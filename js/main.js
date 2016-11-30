5ac4934e917be9622b12c4b1cc094193d07ca6de
sbaidachni <mikedm195@gmail.com>
2016-11-30 15:17:33 -0600

    .

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 console.log(localStorage.getItem("session"));
 if(localStorage.getItem("session") == ''){
+    console.log("out");
     $('#loginBtn').hide();
     $('#logoutBtn').show();
 }else{
+    console.log("in");
     $('#loginBtn').show();
     $('#logoutBtn').hide();
 }
