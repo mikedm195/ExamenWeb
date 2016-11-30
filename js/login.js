0645c3f0748a67497d796049bc67cb9488d0ad39
sbaidachni <mikedm195@gmail.com>
2016-11-30 14:29:49 -0600

    .

--- js/login.js
+++ js/login.js
@@ -17,7 +17,7 @@ function login() {
 		success: function(data, textStatus, jqXHR){
             if(data.vino.length > 0){
                 localStorage.setItem("session", data.vino.id_cliente);
-                window.location = "http://www.yoururl.com";
+                window.location = "index.html";
                 window.location.replace("index.html");
             }else{
                 alert("Usuario y/o contraseña incorrectos");
