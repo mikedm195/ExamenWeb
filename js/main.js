7fd81988c523fce8efdfb7c3a5d190fa7ab46c8b
sbaidachni <mikedm195@gmail.com>
2016-11-30 16:44:48 -0600

    .

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ if(localStorage.getItem("session") != ''){
 }else{
     $('#loginBtn').show();
     $('#logoutBtn').hide();
+    $('#miInformacion').hide();
 }
 
 buscaCliente();
@@ -14,32 +15,32 @@ function logout(){
 
 
 function buscaCliente(){
-    var url = "http://ubiquitous.csf.itesm.mx/~daw-1015019/content/ExamenWeb/api/index.php/cliente/" + localStorage.getItem("session");
-    console.log(url);
-    $.ajax({
-		type: 'GET',
-		url: url,
-		dataType: "json",
-		success: function(data, textStatus, jqXHR){
-            var cliente = data;
-            console.log(cliente);
-            console.log(data);
-            if(cliente){
-                $('#nombreCliente').val(cliente.nombre);
-            	$('#apPaternoCliente').val(cliente.apPaterno);
-            	$('#apMaternoCliente').val(cliente.apMaterno);
-            	$('#telefonoCliente').val(cliente.telefono);
-            	$('#membresiaCliente').val(cliente.membresia);
-                $('#userCliente').val(cliente.user);
-            	$('#passwordCliente').val(cliente.password);
-            }else{
-                alert("Usuario y/o contraseña incorrectos");
-            }
-		},
-		error: function(jqXHR, textStatus, errorThrown){
-			alert('Error en la funcion getCliente: ' + textStatus);
-		}
-	});
+    if(localStorage.getItem("session") != ''){
+        var url = "http://ubiquitous.csf.itesm.mx/~daw-1015019/content/ExamenWeb/api/index.php/cliente/" + localStorage.getItem("session");
+
+        $.ajax({
+    		type: 'GET',
+    		url: url,
+    		dataType: "json",
+    		success: function(data, textStatus, jqXHR){
+                var cliente = data;
+                if(cliente){
+                    $('#nombreCliente').val(cliente.nombre);
+                	$('#apPaternoCliente').val(cliente.apPaterno);
+                	$('#apMaternoCliente').val(cliente.apMaterno);
+                	$('#telefonoCliente').val(cliente.telefono);
+                	$('#membresiaCliente').val(cliente.membresia);
+                    $('#userCliente').val(cliente.user);
+                	$('#passwordCliente').val(cliente.password);
+                }else{
+                    alert("Usuario y/o contraseña incorrectos");
+                }
+    		},
+    		error: function(jqXHR, textStatus, errorThrown){
+    			alert('Error en la funcion getCliente: ' + textStatus);
+    		}
+    	});
+    }
 }
 
 function obtenerDatosCliente() {
@@ -55,7 +56,6 @@ function obtenerDatosCliente() {
 
 function  actualizarCliente(){
     var url = "http://ubiquitous.csf.itesm.mx/~daw-1015019/content/ExamenWeb/api/index.php/cliente/" + localStorage.getItem("session");
-    console.log('updateCliente');
 	$.ajax({
 		type: 'PUT',
 		contentType: 'application/json',
