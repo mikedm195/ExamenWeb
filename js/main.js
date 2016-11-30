f0933d8a5f3076101b361a7b9af342f89e9acfe5
sbaidachni <mikedm195@gmail.com>
2016-11-30 15:42:53 -0600

    .

--- js/main.js
+++ js/main.js
@@ -2,11 +2,35 @@
 if(localStorage.getItem("session") != ''){
     $('#loginBtn').hide();
     $('#logoutBtn').show();
-}else{    
+}else{
     $('#loginBtn').show();
     $('#logoutBtn').hide();
 }
 
 function logout(){
     localStorage.setItem("session", '');
+}
+
+function buscaCliente(){
+    $.ajax({
+		type: 'GET',
+		contentType: 'application/json',
+		url: rootURL,
+		dataType: "json",
+		data: formToJSON(),
+		success: function(data, textStatus, jqXHR){
+            if(data.vino.length > 0){
+                localStorage.setItem("session", data.vino[0].id_cliente);
+                //alert(localStorage.getItem("session"));
+                window.location = "index.html";
+            }else{
+                alert("Usuario y/o contraseña incorrectos");
+            }
+			//$('#btnBorrar').show();
+			//$('#Idvino').val(data.id);
+		},
+		error: function(jqXHR, textStatus, errorThrown){
+			alert('Error en la funcion agregaVino: ' + textStatus);
+		}
+	});
 }
