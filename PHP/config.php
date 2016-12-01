267b7c2dc728213dbd3dd11b630a6d2628606af1
sbaidachni <mikedm195@gmail.com>
2016-11-30 20:04:55 -0600

    .

--- PHP/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("UserDB", "1020365_user");
-define("PasswordDB", "1020365");
-define("HostDB", "localhost");
-define("NameDB", "daw_1020365");
-?>
---
+++ js/register.js
@@ -0,0 +1,33 @@
+var rootURL = "http://ubiquitous.csf.itesm.mx/~daw-1015019/content/ExamenWeb/api/index.php/login";
+
+function obtenerDatosCliente() {
+	return JSON.stringify({
+            "nombre": $('#nombreCliente').val(),
+            "apPaterno": $('#apPaternoCliente').val(),
+            "apMaterno": $('#apMaternoCliente').val(),
+            "foto": $('#fotoCliente').val(),
+            "telefono": $('#telefonoCliente').val(),
+            "membresia": $('#membresiaCliente').val(),
+            "user": $('#userCliente').val(),
+            "password": $('#passwordCliente').val(),
+		});
+}
+
+function insertUser() {
+
+	$.ajax({
+		type: 'POST',
+		contentType: 'application/json',
+		url: rootURL,
+		dataType: "json",
+		data: obtenerDatosCliente(),
+		success: function(data, textStatus, jqXHR){
+
+            window.location = "index.html";
+
+		},
+		error: function(jqXHR, textStatus, errorThrown){
+			alert('Error al agregar cliente: ' + textStatus);
+		}
+	});
+}
